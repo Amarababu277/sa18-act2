0a199af81e7b3fd60fa71749aade4a0d86b08871
Amarababu277 <158104913+amarababu277@users.noreply.github.com>
2024-04-22 15:04:46 -0500

    config_routes.rb - show.html.erb

---
+++ db_seeds.rb
@@ -0,0 +1,3 @@
+Product.create(name: "Product 1", price: 19.99, description: "Description for Product 1")
+Product.create(name: "Product 2", price: 29.99, description: "Description for Product 2")
+Product.create(name: "Product 3", price: 39.99, description: "Description for Product 3")
